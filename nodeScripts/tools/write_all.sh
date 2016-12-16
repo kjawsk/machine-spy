b3efa0bac66aa09908ef5f84a4c97597ad8f4117
karolkujawski <kjawsk@gmail.com>
2016-12-16 23:30:55 +0100

    added tools

---
+++ nodeScripts/tools/write_all.sh
@@ -0,0 +1,4 @@
+sudo python luatool.py --port /dev/ttyUSB0 --src ../init.lua --dest init.lua --verbose
+sudo python luatool.py --port /dev/ttyUSB0 --src ../credentials.lua --dest credentials.lua --verbose
+sudo python luatool.py --port /dev/ttyUSB0 --src ../main.lua --dest main.lua --verbose
+
