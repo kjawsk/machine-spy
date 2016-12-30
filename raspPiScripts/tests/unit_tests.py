c4fea7a05c66f2fff4caf5ed090e1d007458ce79
karolkujawski <kjawsk@gmail.com>
2016-12-30 11:09:24 +0100

    test for adding entry corrected

--- raspPiScripts/tests/unit_tests.py
+++ raspPiScripts/tests/unit_tests.py
@@ -3,11 +3,12 @@ import datetime
 import sys
 sys.path.append('..')
 from server import app
-from server.models import db
+from server.models import db, Entry
+from server.utils import MyDate
 from flask import json
+from mock import MagicMock
 
-
-class ServerTestCase(unittest.TestCase):
+class PostMessageTestCase(unittest.TestCase):
 
     def setUp(self):
         app.config['TESTING'] = True
@@ -16,23 +17,31 @@ class ServerTestCase(unittest.TestCase):
         self.app = app.test_client()
         db.create_all()
 
-    def test_main_page(self):
-        rv = self.app.get('/', follow_redirects=True)
-
-        self.assertIn('Hello World!', rv.data.decode('utf-8'))
+        self.date = MyDate.now()
+        self.machine_id = 'ESP-1'
+        MyDate.now = MagicMock(return_value=self.date)
+        self.request = json.dumps(dict(machine_id=self.machine_id))
 
     def test_post_message(self):
-        request = json.dumps(dict(
-            machine_id='ESP-1',
-            date=datetime.datetime.now()))
-
         rv = self.app.post(
             '/entries',
-            data=request,
+            data=self.request,
             content_type='application/json')
 
-        self.assertNotIn('415 Unsupported Media Type', rv.data.decode('utf-8'))
         self.assertIn('Entry added', rv.data.decode('utf-8'))
+        self.assertNotIn('415 Unsupported Media Type', rv.data.decode('utf-8'))
+        entries = db.session.query(Entry)
+        self.assertEqual(entries[0].date, self.date)
+        self.assertEqual(entries[0].machine_id, self.machine_id)
+
+    def test_post_message_with_invalid_header(self):
+        rv = self.app.post(
+            '/entries',
+            data=self.request,
+            content_type='INVALID HEADER')
+
+        self.assertNotIn('Entry added', rv.data.decode('utf-8'))
+        self.assertIn('415 Unsupported Media Type', rv.data.decode('utf-8'))
 
     def tearDown(self):
         db.session.remove()
