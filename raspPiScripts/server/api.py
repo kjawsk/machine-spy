883c74bd76ee926cf1ff08e6168052440df7625f
karolkujawski <kjawsk@gmail.com>
2016-12-30 11:13:59 +0100

    stroing to database fixed

--- raspPiScripts/server/api.py
+++ raspPiScripts/server/api.py
@@ -4,6 +4,10 @@ from server.models import db, Entry
 import datetime
 import os
 
+def add_to_db(item):
+    db.session.add(item)
+    db.session.commit()
+
 @app.route('/')
 def index():
     return 'Hello World!'
@@ -13,8 +17,7 @@ def api_entry():
     if request.headers['Content-Type'] == 'application/json':
         content = request.get_json(silent=True)
         new_entry = Entry(content['machine_id'])
-        db.session.add(new_entry)
-        db.session.commit()
+        add_to_db(new_entry)
         return 'Entry added: ' + str(content)
     else:
         return '415 Unsupported Media Type'
