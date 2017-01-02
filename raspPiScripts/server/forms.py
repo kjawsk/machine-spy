a4fadfc43a149f25c0b20e5e21942d9c5c7b88b8
karolkujawski <kjawsk@gmail.com>
2017-01-02 18:16:00 +0100

    validator message for AddSensorForm updated

--- raspPiScripts/server/forms.py
+++ raspPiScripts/server/forms.py
@@ -1,4 +1,9 @@
 from wtforms import Form, StringField, validators
 
 class AddSensorForm(Form):
-    name = StringField('Name', [validators.Length(min=15, max=15)])
+    name = StringField(
+        'Name',
+        [validators.Length(
+            min=15,
+            max=15,
+            message='Name must be 15 character long')])
