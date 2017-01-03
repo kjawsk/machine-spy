949d50c3fcd0abe4a812691b2c23a2ee3fa1ec26
karolkujawski <kjawsk@gmail.com>
2017-01-03 13:03:17 +0100

    added reconnect to the broker after connection lost

--- nodeScripts/main.lua
+++ nodeScripts/main.lua
@@ -7,22 +7,52 @@ INPUT_PIN = 1
 TEST_PIN = 2
 TIMER_TIMEOUT = 5000
 MQTT_KEEPALIVE = 120
+GPIO_TIMER = 0
+RECONNECT_TIMER = 1
+IS_BROKER_CONNECTED = 0
 id1 = 0
 
-function publish_data()
-    m:publish("/test", CLIENTID,0,0, function(conn)
-        print("Sending data: " .. id1)
-        id1 = id1 + 1
-    end)
-end
+-------------------- MQTT HANDLING -----------------------------
 
-function receive_data(client, topic, data)
-    if data ~= nil then
-        print(topic .. " : " ..  data)
+function publish_data()
+    id1 = id1 + 1
+    if IS_BROKER_CONNECTED == 1 then
+        m:publish("/test", CLIENTID,0,0, function(conn)
+            print("Sending data: " .. id1)
+        end)
     end
 end
 
+function handle_broker_offline(client)
+    print ("Connection to the broker is lost. Reconnecting...")
+    IS_BROKER_CONNECTED = 0
+    m:close()
+
+    tmr.alarm(RECONNECT_TIMER, TIMER_TIMEOUT, tmr.ALARM_AUTO, connect_to_broker)
+end
+
+function handle_connection_error (client, reason)
+    print("Failed reason: "..reason)
+end
+
+function connect_to_broker()
+    print ("Waiting for the broker")
+    m:connect(
+        BROKER, BRPORT, 0,
+        function (client)
+            print("Connected to MQTT:" .. BROKER .. ":" .. BRPORT .." as " .. CLIENTID )
+            tmr.unregister(RECONNECT_TIMER)
+            IS_BROKER_CONNECTED = 1
+        end,
+        handle_connection_error
+    )
+    m:on("offline", handle_broker_offline)
+end
+
+-------------------- GPIO HANDLING -----------------------------
+
 function toggle_input()
+    print("Input toggled")
     if gpio.read(INPUT_PIN) == 1 then
         gpio.write(TEST_PIN, gpio.LOW)
     else
@@ -30,24 +60,20 @@ function toggle_input()
     end
 end
 
-function run_main_prog()
-    print("Connected to MQTT:" .. BROKER .. ":" .. BRPORT .." as " .. CLIENTID )
+function gpio_handling()
+    print("GPIO handling started")
 
     gpio.mode(INPUT_PIN, gpio.INT)
     gpio.trig(INPUT_PIN, "up", publish_data)
 
     -- it is only for testing purposes - toggle input every TIMER_TIMEOUT
     gpio.mode(TEST_PIN, gpio.OUTPUT)
-    tmr.alarm(1, TIMER_TIMEOUT, tmr.ALARM_AUTO, toggle_input)
-
-    m:subscribe("/ping",0, function(client) print("Subscribe success") end)
-    m:on("message", receive_data)
+    tmr.alarm(GPIO_TIMER, TIMER_TIMEOUT, tmr.ALARM_AUTO, toggle_input)
 end
 
-function handle_connection_error (client, reason)
-    print("Failed reason: "..reason)
-end
+----------------------------------------------------------------
 
 print "Connecting to MQTT broker. Please wait..."
 m = mqtt.Client( CLIENTID, MQTT_KEEPALIVE, BRUSER, BRPWD)
-m:connect( BROKER , BRPORT, 0, run_main_prog, handle_connection_error)
+connect_to_broker()
+gpio_handling()
