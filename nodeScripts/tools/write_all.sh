b8897580ee094270be780209bfa6a6a899e20939
karolkujawski <kjawsk@gmail.com>
2017-02-12 20:36:03 +0100

    tools folder removed from repo

--- nodeScripts/tools/write_all.sh
+++
@@ -1,4 +0,0 @@
-sudo python luatool.py --port /dev/ttyUSB1 --src ../init.lua --dest init.lua --verbose
-sudo python luatool.py --port /dev/ttyUSB1 --src ../credentials.lua --dest credentials.lua --verbose
-sudo python luatool.py --port /dev/ttyUSB1 --src ../main.lua --dest main.lua --verbose
-
